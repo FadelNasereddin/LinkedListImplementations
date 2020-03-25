7902f4fc57d727c857fc9b1609d5bb09515f8d93
FadelNasereddin <50805022+FadelNasereddin@users.noreply.github.com>
2020-03-24 20:27:06 -0400

    created an insertAtStart() method to create a new head node

--- LinkedList.java
+++ LinkedList.java
@@ -39,4 +39,12 @@ public class LinkedList {
 		
 		return counter;
 	}
-	
+    
+    public void insertAtStart(int data){
+		Node node = new Node ();
+		node.data=data;
+		node.next=null;
+		node.next= head;
+		head=node;
+		
+	}
