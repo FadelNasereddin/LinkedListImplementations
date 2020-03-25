3712043be3cdc8be23cb8ae21600b758f5350ffb
FadelNasereddin <50805022+FadelNasereddin@users.noreply.github.com>
2020-03-24 20:32:44 -0400

    created a getByIndex() method to retrieve a node by index

--- LinkedList.java
+++ LinkedList.java
@@ -47,4 +47,30 @@ public class LinkedList {
 		node.next= head;
 		head=node;
 		
+    }
+    
+    public void insertAt(int index,int data){
+		Node node = new Node();
+		node.data= data;
+		node.next=null;
+		Node traverse=head;
+		
+		if(index==0){
+			insertAtStart(data);
+		}
+		else{
+		for(int i=0;i<index-1;i++){
+			traverse=traverse.next;
+		}
+		node.next=traverse.next;
+		traverse.next= node;
+	}
+    }
+    
+    public void getByIndex(int index){
+		Node traverse = head;
+		for(int i=0; i<index;i++){
+			traverse=traverse.next;
+		}
+		System.out.println(traverse.data);
 	}
